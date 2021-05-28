3024c52a524c3f82ea527a71d25af21ea5d31822
spineki <marras.antoine@gmail.com>
2021-05-28 13:45:59 +0200

    feat: first commit, simple configuration for typesscript and graphql

---
+++ app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./schema/schema";
+
+
+const app = express();
+
+
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log("Now listening for request on port 4000");
+})
---
+++ schema/schema.ts
@@ -0,0 +1,39 @@
+import { GraphQLString, GraphQLObjectType, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } from "graphql";
+
+// dummy data
+const examples = [
+    {name: "ex1", id: "1" },
+    {name: "ex2", id: "2" },
+    {name: "ex3", id: "3" },
+    {name: "ex4", id: "4" }
+];
+
+const ExampleType: GraphQLObjectType = new GraphQLObjectType({
+    name: 'Example',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        example: {
+            type: ExampleType,
+            args: {
+                id: {type: GraphQLID}
+            },
+            resolve(parent, args) {
+                const matchingExample = examples.find((element) => element.id === args.id );
+                return matchingExample;
+            }
+        }
+    }
+});
+
+
+export const schema = new GraphQLSchema({
+    query: RootQuery 
+});
+
